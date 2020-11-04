6a6dde339af6a4a15bfba9ca6c99fec79dc1ca6e
tkawabata-g <t.kawabata20@gmail.com>
2020-11-04 13:18:53 +0900

    commit

---
+++ android/app/src/main/kotlin/com/example/push_push/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.push_push
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+class MainActivity: FlutterActivity() {
+    
+}
